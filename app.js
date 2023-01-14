cec50679953e7b7440e5cb4aba1e5c7bc61feafe
Brice <brice@rocknmeet.com>
2023-01-14 17:52:55 +0100

    Added Dockerfile and updated README

--- app.js
+++ app.js
@@ -4,27 +4,13 @@ const app = express();
 const dotenv = require('dotenv');
 // use dotenv to load environment variables from .env file
 dotenv.config();
-const port = process.env.PORT || 3000;
-const MONGODB_URL = process.env.MONGODB_URL;
-const mongoose = require('mongoose');
+const port = process.env.EXPRESS_PORT || 3000;
 
-// Connect to MongoDB
-mongoose.connect(MONGODB_URL, {
-}).then(() => {
-    console.log('Connected to MongoDB');
-    // Console log the database name
-    console.log('Database name: ' + mongoose.connection.name);
-}
-).catch((err) => {
-    console.log(err);
-}
-);
 
 // SIMPLE INDEX WHO SERVE A INDEX.HTML IN THE PUBLIC FOLDER
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
-
 
 // Start express server
 app.listen(port, () => {
